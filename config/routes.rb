9d82a912f26b03df3e48cc2d3a6ddd656a828382
Muhammad Jalabi <modi.jalabi@gmail.com>
2020-11-15 14:47:55 +0100

    add bootstrap and about routes + nav

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  #get 'home/index'
+  get 'home/about'
+
+
+  root 'home#index' 
+  #sets the root-folder(index)
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
